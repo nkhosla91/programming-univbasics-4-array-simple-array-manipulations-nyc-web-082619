0410565a2af268481045ffaa4200ca3263471e93
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-12 20:04:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,3 +29,15 @@ end
 def using_insert(array, element)
   array.insert(4, element)
 end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete(integer)
+end
