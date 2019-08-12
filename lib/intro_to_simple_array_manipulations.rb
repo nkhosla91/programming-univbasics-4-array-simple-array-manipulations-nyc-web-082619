46b31b18a78cce5c82c762e00c26f35197214d2f
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-12 19:57:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,8 +22,6 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-def using_concat(my_favorite_things, more_favs)
-  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["mario kart", "flatiron school"]
-  @all_my_favs = @my_favorite_things.concat(more_favs)
+def using_insert(array1, array2)
+  array1 = array1.concat(array2)
 end
